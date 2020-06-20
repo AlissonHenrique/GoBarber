2c31a3c6578996805509fb2516527e757c75a271
Alisson Henrique <alisson-henri@live.com>
2020-06-20 11:31:54 -0300

    init project

---
+++ appgo/src/App.tsx
@@ -0,0 +1,27 @@
+//import 'react-native-gesture-handler';
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+//import Routes from './routes/auth.routes';
+//import { NavigationContainer } from '@react-navigation/native';
+//import AppProvider from './hooks';
+const App: React.FC = () => (
+    <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+ 
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+ 
+      </View>
+      </>
+);
+
+export default App;
+// const App: React.FC = () => (
+//     // <NavigationContainer>
+//        <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+//       // <AppProvider>
+//          <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+//         {/* <Routes /> */}
+//          </View>
+//      //  </AppProvider>
+//     // </NavigationContainer>
+//    );
