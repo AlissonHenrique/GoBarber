db85f19e813619582f386cac1f9e40141aefc4f9
Alisson Henrique <alisson-henri@live.com>
2020-07-01 17:22:53 -0300

    ajsutes para deploy

---
+++ backend/babel.config.js
@@ -0,0 +1,23 @@
+const { eachYearOfInterval } = require('date-fns');
+
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@modules': './src/modules',
+          '@config': './src/config',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
